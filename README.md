5eaf883394046c03ae3bed7276c09ea19f6e878c
Josue Gomez <jgomez13@terpmail.umd.edu>
2016-12-22 16:00:25 -0500

    working requests

--- README.md
+++ README.md
@@ -5,3 +5,22 @@
 - mongoimport --db myUberApp --collection policeData --drop --file ./uberConcept.json
 - db.policeData.find().pretty()
 - db.policeData.find({userId:“01”}).pretty()
+- db.policeData.createIndex({"location":"2dsphere"})
+- Now, to get all cops that are located within 2 kilometers from latitude 12.9718915 and longitude 77.64115449999997, run this :
+```
+db.policeData.find({
+    location: {
+        $near: {
+            $geometry: {
+                type: "Point",
+                coordinates: [77.64115449999997, 12.9718915]
+            },
+            $maxDistance: 2000
+        }
+    }
+}).pretty()
+```
+- sudo npm install
+To Run:
+
+- node app.js 
--- routes.js
+++ routes.js
@@ -1,8 +1,22 @@
+var dbOperations = require('./db-operations');
+
 function initialize(app, db) { 
     //A GET request to /cops should return back the nearest cops in the vicinity.
     app.get('/cops', function(req, res){
-    /*extract the latitude and longitude info from the request. Then, fetch the nearest cops using MongoDB's geospatial queries and return it back to the client.
+    /*extract the latitude and longitude info from the request. 
+        Then, fetch the nearest cops using MongoDB's geospatial queries and 
+        return it back to the client.
     */
+        //Convert the query strings into Numbers
+        var latitude = Number(req.query.lat);
+        var longitude = Number(req.query.lng);
+
+        dbOperations.fetchNearestCops(db, [longitude,latitude], function(results){
+        //return the results back to the client in the form of JSON
+            res.json({
+                cops: results
+            });
+        });  
     });
 }
 exports.initialize = initialize;
